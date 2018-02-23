9ca5c0a1c183123197d73821c11c1b01d9741557
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 11:21:48 -0500

    initial

---
+++ Staircase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            string printString = "";
+            int numOfSpaces = n - i;
+            int numOfHash = n - numOfSpaces;
+            for (int s = 0; s < numOfSpaces; s++)
+            {
+                printString += " ";
+            }
+            for (int a = 0; a < numOfHash; a++)
+            {
+                printString += "#";
+            }
+                Console.WriteLine(printString);
+            
+        }
+
+       
+        Console.ReadLine();
+
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        staircase(n);
+    }
+}
